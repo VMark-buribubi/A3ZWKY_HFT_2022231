03813c264fbf7c6a8d321da56c6f72bd1dcdfe06
VMark - buribubi <plants.mark@gmail.com>
2022-09-28 14:14:52 +0200

    Add project files.

---
+++ A3ZWKY_HFT_2022231.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace A3ZWKY_HFT_2022231.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ A3ZWKY_HFT_2022231.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace A3ZWKY_HFT_2022231.Models
+{
+    public class Class1
+    {
+    }
+}
