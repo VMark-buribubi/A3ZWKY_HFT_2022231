64ff2dff2fd4d6bc5df14f640770f7501f28c24c
VMark-buribubi <plants.mark@gmail.com>
2022-11-21 18:11:30 +0100

    Model classes done (empty)

---
+++ A3ZWKY_HFT_2022231.Models/House.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A3ZWKY_HFT_2022231.Models
+{
+    public class House
+    {
+
+    }
+}
