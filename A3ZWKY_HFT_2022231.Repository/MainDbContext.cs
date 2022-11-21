448057d789c6923ec338eb61067217a3f0bd7d09
VMark-buribubi <plants.mark@gmail.com>
2022-11-21 23:47:18 +0100

    DbContext and OnConfiguring done

---
+++ A3ZWKY_HFT_2022231.Repository/MainDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using A3ZWKY_HFT_2022231.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace A3ZWKY_HFT_2022231.Repository
+{
+    internal class MainDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<House> Houses { get; set; }
+        public DbSet<Workplace> Workplaces { get; set; }
+
+        public MainDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if (!builder.IsConfigured)
+            {
+                string conn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Main.mdf;Integrated Security=True;MultipleActiveResultSets=true";
+                builder
+                    .UseLazyLoadingProxies()
+                    .UseSqlServer(conn);
+            }
+        }
+
+
+    }
+}
