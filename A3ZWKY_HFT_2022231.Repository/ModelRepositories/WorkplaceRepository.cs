9523be7218b2362a00831ef253f45d50a0707d95
VMark-buribubi <plants.mark@gmail.com>
2022-11-22 02:47:09 +0100

    Added Modelrepositories

---
+++ A3ZWKY_HFT_2022231.Repository/ModelRepositories/WorkplaceRepository.cs
@@ -0,0 +1,31 @@
+﻿using A3ZWKY_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A3ZWKY_HFT_2022231.Repository.ModelRepositories
+{
+    public class WorkplaceRepository : Repository<Workplace>, IRepository<Workplace>
+    {
+        public WorkplaceRepository(MainDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Workplace Read(int id)
+        {
+            return ctx.Workplaces.FirstOrDefault(x => x.WorkplaceId == id);
+        }
+
+        public override void Update(Workplace item)
+        {
+            var old = Read(item.WorkplaceId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
