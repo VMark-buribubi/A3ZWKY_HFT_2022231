1a57ea33038486aab54d9f007b6e2c8a3a938887
VMark-buribubi <plants.mark@gmail.com>
2022-11-27 23:42:27 +0100

    WorkplaceController done

--- A3ZWKY_HFT_2022231.Endpoint/WorkplaceController.cs
+++ A3ZWKY_HFT_2022231.Endpoint/WorkplaceController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using A3ZWKY_HFT_2022231.Logic;
+using A3ZWKY_HFT_2022231.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace A3ZWKY_HFT_2022231.Endpoint
 {
@@ -6,6 +9,39 @@ namespace A3ZWKY_HFT_2022231.Endpoint
     [ApiController]
     public class WorkplaceController : ControllerBase
     {
+        IWorkplaceLogic logic;
 
+        public WorkplaceController(IWorkplaceLogic logic)
+        {
+            this.logic = logic;
+        }
+        [HttpPost("/workplaces")]
+        public void Create([FromBody] Workplace item, [FromRoute] int workplaceId)
+        {
+            this.logic.Create(item, workplaceId);
+        }
+        [HttpDelete("/workplace/{workplaceId}")]
+        public void Delete([FromRoute] int id)
+        {
+            this.logic.Delete(id);
+        }
+
+        [HttpGet("/workplaces/{workplaceId}")]
+        public Workplace Read([FromRoute] int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpGet("/workplaces")]
+        public IQueryable<Workplace> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpPut("/workplaces/{workplaceId}")]
+        public void Update([FromBody] Workplace item, [FromRoute] int workplaceId)
+        {
+            this.logic.Update(item, workplaceId);
+        }
     }
 }
